7bcba9c7ed70e4229b16093a82abc6b140b448c8
Kuro Da Poet <nguyentaikurosuna@gmail.com>
2026-05-02 16:14:06 +0700

    Add build.bat and ignore OUT directory
    Add a Windows batch script (build.bat) to run Unreal's RunUAT BuildPlugin to package the UnrealClaude plugin for Win64 and place the package in an OUT directory. Update .gitignore to exclude the generated OUT directory so build artifacts are not committed.

---
+++ build.bat
@@ -0,0 +1,1 @@
+D:\Epic\UE_5.7\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="D:\_UE\UnrealClaude\UnrealClaude\UnrealClaude.uplugin" -Package="D:\_UE\UnrealClaude\OUT" -TargetPlatforms=Win64
